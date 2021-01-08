913b4d92050acf307069c419973970535e14c684
abh224272 <abh224272@gmail.com>
2021-01-08 17:46:47 +0530

    [Abhishek Mishra] Add: Created an object of Linled list Class and added values to it

---
+++ LinkedListSolution/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LinkedListSolution
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList<int> linkedList = new LinkedList<int>();
+            linkedList.AddLast(56);
+            linkedList.AddLast(30);
+            linkedList.AddLast(70);
+
+            foreach(int value in linkedList)
+            {
+                Console.Write(value + " ");
+            }
+        }
+    }
+}
