ce234a6ab0e285eab15508c3c9981865faddbd09
abh224272 <abh224272@gmail.com>
2021-01-08 22:03:42 +0530

    [Abhishek Mishra] Add: Test Method to check the ability of Insert At Particular Position of Linked List

--- LinkedListNUnitTestProject/LinledListTestCaseClass.cs
+++ LinkedListNUnitTestProject/LinledListTestCaseClass.cs
@@ -18,5 +18,30 @@ namespace LinkedListNUnitTestProject
             int searchOutput = linkedListOperations.Search(30);
             Assert.AreEqual(1, searchOutput);
         }
+        [Test]
+        public void GivenValue_WhenSearchLinkedList_ShouldReturnTheListWithValueInsertedNextToTheSearchPosition()
+        {
+            LinkedListOperations list1 = new LinkedListOperations();
+            int list1Count = 0, list2Count = 0;
+            list1.AddAtHead(56); list1.AddAtHead(30); list1.AddAtHead(40); list1.AddAtHead(70);
+            LinkedListOperations list2 = new LinkedListOperations();
+            list2.AddAtHead(56); list2.AddAtHead(30); list2.AddAtHead(70);
+            int searchOutput = list2.Search(30);
+            list2.InsertAtParticularPosition(searchOutput + 1, 40);
+            Node temp1 = list1.head;
+            Node temp2 = list2.head;
+            while (temp1 != null)
+            {
+                temp1 = temp1.next;
+                list1Count++;
+            }
+            while (temp2 != null)
+            {
+                temp2 = temp2.next;
+                list2Count++;
+            }
+            Assert.AreEqual(list1Count, list2Count);
+        }
+
     }
 }
